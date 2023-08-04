463c0a6ddb44c7e07fccb70ef5603884641bca77
jonsxscar <jonsxscar@gmail.com>
2023-08-04 12:14:29 -0500

    borrado de cosas

--- src/components/about/About.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function About() {
-  return <div><h1>Esta es la app de Rick & Morty. Creada por: "Jonnathan"</h1></div>;
-}
-
-export default About;
-
--- src/redux/action_types.js
+++
@@ -1,6 +0,0 @@
-export const ADD_FAV = 'ADD_FAV';
-export const REMOVE_FAV = 'REMOVE_FAV';
-export const FILTER_CARDS = 'FILTER_CARDS';
-export const ORDER_CARDS = 'ORDER_CARDS';
-
-//cuando hago la action_types me evito usar '' en las actions
