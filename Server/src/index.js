d9cae4c17e3fcb69e665f5797dbcbad03461a0cd
jonsxscar <jonsxscar@gmail.com>
2023-08-11 22:59:51 -0500

    orm1

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,18 @@
+const { conn } = require('./DB_connection')
 const server = require('./app');
+require('dotenv').config()
 
-const PORT = 3001;
+const {PORT} = process.env;
 
-server.listen(PORT, () => {
+//const PORT = 3001;
+
+server.listen(PORT, async ()=>{
+  await conn.sync({force:true})
+  console.log(`Server is listening on port: ${PORT}`);
+})
+
+
+/* server.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
+ */
